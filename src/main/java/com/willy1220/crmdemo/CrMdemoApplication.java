73ddfc1bed683ca1bec961655c4dd58e3e570da8
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2025-06-11 17:14:27 +0800

    JPA-DB setting

---
+++ src/main/java/com/willy1220/crmdemo/CrMdemoApplication.java
@@ -0,0 +1,13 @@
+package com.willy1220.crmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrMdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrMdemoApplication.class, args);
+	}
+
+}
